d8c580516f3f58c563848397485dcf9a5a468423
hyemin <88538911+hhyem@users.noreply.github.com>
2023-01-20 12:56:38 +0900

    1차 마이페이지(정보 수정), 메인화면(날씨), 산책로, 쓰레기통 위치 액티비티, xml 추가

---
+++ app/src/main/java/com/example/guru2/AppMain.kt
@@ -0,0 +1,11 @@
+package com.example.guru2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AppMain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app_main)
+    }
+}
