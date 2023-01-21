9e4e3a66e2d9bb3a4a3e47341a0c1ab5629ef9a5
ukkiyeon <ukkiyeon@naver.com>
2023-01-21 12:32:09 +0900

    tip.xml, Tip.kt 재생성

--- app/src/main/java/com/example/guru2/Tip.kt
+++ app/src/main/java/com/example/guru2/Tip.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class Tip : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tip)
+        setContentView(R.layout.tip)
     }
 }
