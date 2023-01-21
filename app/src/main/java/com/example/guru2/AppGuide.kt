38316b3a0ca0bd27ab3679d369dfed5056f74ec7
ukkiyeon <ukkiyeon@naver.com>
2023-01-21 16:09:09 +0900

    fragment_tip.xml, app_guide.xml 생성

---
+++ app/src/main/java/com/example/guru2/AppGuide.kt
@@ -0,0 +1,11 @@
+package com.example.guru2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AppGuide : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.app_guide)
+    }
+}
--- app/src/main/java/com/example/guru2/MainActivity.kt
+++ app/src/main/java/com/example/guru2/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.guru2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var containers : FrameLayout
+    lateinit var bottom_navigationview : BottomNavigationMenuView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        containers = findViewById(R.id.containers)
+        bottom_navigationview = findViewById(R.id.bottom_navigationview)
+
+        supportFragmentManager.beginTransaction().add(containers.id, MainFragment()).commit()
+
+
     }
 }
