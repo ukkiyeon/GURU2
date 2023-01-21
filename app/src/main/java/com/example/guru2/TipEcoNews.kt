3ccd7e444480746cd4a095f6d578e3196a47bcf7
ukkiyeon <ukkiyeon@naver.com>
2023-01-21 19:26:00 +0900

    TIP 파일들 생성

---
+++ app/src/main/java/com/example/guru2/TipEcoNews.kt
@@ -0,0 +1,11 @@
+package com.example.guru2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TipEcoNews : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tip_eco_news)
+    }
+}
---
+++ app/src/main/java/com/example/guru2/TipFlogging.kt
@@ -0,0 +1,11 @@
+package com.example.guru2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TipFlogging : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tip_flogging)
+    }
+}
