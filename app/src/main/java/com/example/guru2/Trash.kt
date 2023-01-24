b85601f4c2d5a49a9ff23fb19cf107a5424351c0
hyemin <88538911+hhyem@users.noreply.github.com>
2023-01-24 16:54:50 +0900

    지도 추가

--- app/src/main/java/com/example/guru2/Trash.kt
+++ app/src/main/java/com/example/guru2/Trash.kt
@@ -7,15 +7,26 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class Trash : AppCompatActivity() {
 
-    lateinit var btn_weather:Button
-    lateinit var btn_walk:Button
+    lateinit var btn_weather: Button
+    lateinit var btn_walk: Button
+
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.trash)
+
+        val mapFragment: SupportMapFragment =
+            supportFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         btn_weather = findViewById(R.id.btn_weather)
         btn_walk = findViewById(R.id.btn_walk)
@@ -50,6 +61,16 @@ class Trash : AppCompatActivity() {
         btn_communityPage.setOnClickListener {
             startActivity(Intent(this@Trash, CommunityMain::class.java))
         }
-
     }
+
+    fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val marker = LatLng(35.241615, 128.695587)
+        mMap.addMarker(MarkerOptions().position(marker).title("마커 제목"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
+    }
+}
+
+private fun SupportMapFragment.getMapAsync(trash: Trash) {
+
 }
--- app/src/main/java/com/example/guru2/Walk.kt
+++ app/src/main/java/com/example/guru2/Walk.kt
@@ -7,15 +7,26 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class Walk : AppCompatActivity() {
 
     lateinit var btn_weather: Button
     lateinit var btn_trash: Button
 
+    private lateinit var mMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.walk)
+
+        val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.mapview) as SupportMapFragment
+        mapFragment.getMapAsync(this)
 
         btn_weather = findViewById(R.id.btn_weather)
         btn_trash = findViewById(R.id.btn_trash)
@@ -52,4 +63,14 @@ class Walk : AppCompatActivity() {
         }
     }
 
+    fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        val marker = LatLng(35.241615, 128.695587)
+        mMap.addMarker(MarkerOptions().position(marker).title("마커 제목"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(marker))
+    }
 }
+
+    private fun SupportMapFragment.getMapAsync(walk: Walk) {
+
+    }
